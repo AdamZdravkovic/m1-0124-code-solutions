afefb343298926397940986529bd530999d51d55
Adam Zdravkovic <adambusiness24@gmail.com>
2024-02-01 06:44:20 +0000

    TypeScript Operators and Expressions - Adam Zdravkovic

---
+++ typescript-operators-and-expressions/main.ts
@@ -0,0 +1,65 @@
+const width: number = 55;
+
+const height: number = 23;
+
+const area: number = width * height;
+
+console.log('value of area: ' + area);
+
+console.log('typeof area: ' + typeof area);
+
+const bill: number = 11;
+
+const payment: number = 21;
+
+const change: number = bill - payment;
+
+console.log('value of change: ' + change);
+
+console.log('typeof change: ' + typeof change);
+
+const quizzes: number = 2;
+
+const midterm: number = 5;
+
+const final: number = 7;
+
+const grade: number = (quizzes + midterm + final) / 3;
+
+console.log('value of grade: ' + grade);
+
+console.log('typeof grade: ' + typeof grade);
+
+const firstName: string = 'Adam ';
+
+const lastName: string = 'Zdravkovic';
+
+const fullName: string = firstName + lastName;
+
+console.log('value of fullName: ' + fullName);
+
+console.log('typeof fullName: ' + typeof fullName);
+
+const pH: number = 8;
+
+const isAcidic: boolean = pH < 7;
+
+console.log('value of isAcidic: ' + isAcidic);
+
+console.log('typeof isAcidic: ' + typeof isAcidic);
+
+const headCount: number = 15;
+
+const isSparta: boolean = headCount === 300;
+
+console.log('value of isSparta: ' + isSparta);
+
+console.log('typeof isSparta: ' + typeof isSparta);
+
+let motto: string = fullName;
+
+motto += ' is the GOAT';
+
+console.log('value of motto: ' + motto);
+
+console.log('typeof motto: ' + typeof motto);
