037455b248851de2e0de665f014a9a6cc3153580
Adam Zdravkovic <adambusiness24@gmail.com>
2024-02-06 00:34:29 +0000

    TypeScript Data Model - Adam Zdravkovic

---
+++ typescript-data-model/order-history.js
@@ -0,0 +1,73 @@
+'use strict';
+const orderHistory = [
+  {
+    orderDate: 'August 4, 2020',
+    totalAmount: 34.0,
+    address: 'JS Masher',
+    orderNumber: '114 - 3941689 - 8772232',
+    dateDelivery: 'Delivered Aug 8, 2020',
+    items: [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauscmayer, Dr. Axel',
+        returnWindow: 'Return window closed on Sep 7, 2020',
+        amount: 31.55,
+      },
+    ],
+  },
+  {
+    orderDate: 'July 19, 2020',
+    totalAmount: 44.53,
+    address: 'JS Masher',
+    orderNumber: '113 - 9984268 - 1280257',
+    dateDelivery: 'Delivered Jul 20, 2020',
+    deliveryDescription:
+      'Your package was delivered. It was handed directly to a resident.',
+    items: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindow: 'Return window closed on Aug 19, 2020',
+        amount: 41.33,
+      },
+    ],
+  },
+  {
+    orderDate: 'July 4, 2020',
+    totalAmount: 17.22,
+    address: 'JS Masher',
+    orderNumber: '114 - 2875557 - 9059409',
+    dateDelivery: 'Delivered Jul 7, 2020',
+    deliveryDescription:
+      'Your package was delivered. It was handed directly to a resident.',
+    items: [
+      {
+        title:
+          'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        returnWindow: 'Return window closed on Aug 5, 2020',
+        amount: 15.98,
+      },
+    ],
+  },
+  {
+    orderDate: 'July 3, 2020',
+    totalAmount: 138.93,
+    orderNumber: '113 - 2883177 - 2648248',
+    dateDelivery: 'Delivered Jul 5, 2020',
+    address: 'JS Masher',
+    items: [
+      {
+        title:
+          'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        amount: 94.95,
+        returnWindow: 'Return window closed on Aug 4, 2020',
+      },
+      {
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        amount: 33.99,
+        returnWindow: 'Return window closed on Aug 4, 2020',
+      },
+    ],
+  },
+];
